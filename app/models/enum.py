18b602bc6a73ea2c7b32b75e3d61be5662347197
Ifeoluwa <os.ifeoluwa@gmail.com>
2025-03-24 15:29:18 +0100

    Scalfolding the project structure

---
+++ app/models/enum.py
@@ -0,0 +1,19 @@
+from enum import Enum, auto
+
+class PhoneType(Enum):
+    MOBILE = auto()
+    HOME = auto()
+    WORK = auto()
+    OTHER = auto()
+
+
+class EmailType(Enum):
+    PERSONAL = auto()
+    WORK = auto()
+    OTHER = auto()
+
+
+class AddressType(Enum):
+    HOME = auto()
+    WORK = auto()
+    OTHER = auto()
